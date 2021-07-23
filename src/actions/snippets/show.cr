506e2b4502c37a162ed35a8f7c6e1f55f5b4a594
Stephen Dolan <dolan.stephen1@gmail.com>
2021-07-23 14:34:46 -0400

    Add more elegant redirects for Snippets::Show

--- src/actions/snippets/show.cr
+++ src/actions/snippets/show.cr
@@ -1,7 +1,20 @@
 class Snippets::Show < BrowserAction
   get "/snippets/:snippet_id" do
-    snippet = SnippetQuery.new.creator_id(current_user.id).find(snippet_id)
+    snippet = SnippetQuery.new.find(snippet_id)
 
-    html Snippets::ShowPage, snippet: snippet
+    # If you created the snippet, show the snippet
+    # If you aren't allowed access, go away
+    # If you already submitted a revision, show the revision
+    # Otherwise, you're probably trying to create a revision
+    if snippet.creator == current_user
+      html Snippets::ShowPage, snippet: snippet
+    elsif snippet.domain_restricted? && (snippet.creator.email_domain != current_user.email_domain)
+      flash.failure = "That snippet is private."
+      redirect Home::Index
+    elsif (revision = snippet.revisions.find { |rev| rev.creator == current_user })
+      redirect to: Snippets::Revisions::Show.with(snippet_id: snippet.slug, revision_id: revision.id)
+    else
+      redirect to: Snippets::Revisions::New.with(snippet_id: snippet.slug)
+    end
   end
 end
